9b63e1600853a82133578121dd4092d76fa05141
tupham2006 <tupham20061995@gmail.com>
2017-12-12 13:22:34 +0700

    x

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -5,34 +5,33 @@ const webpush = require('web-push');
 module.exports = {
 	register: function (req, res) {
 		var key = req.param('key');
-		console.log(key);
-		return res.json({key: key});
 	},
+
 	send: function(req, res){
 		const vapidKeys = webpush.generateVAPIDKeys();
 		console.log(vapidKeys);
 		return res.json({vapidKeys: vapidKeys});
-		
-		var pushSubscription = {
-			endpoint:"https://fcm.googleapis.com/fcm/send/dhwptTziGQA:APA91bFzuQNiHeslyZm5VHQ9flDrgwf0OKG5okaS9CfPynJtXHiOMyLORqBtbflbBpgT1MQHiRtEFLhWwBd-A851vgFV5iOoZbKqRbZwugngRB-enuJWe-n-IClsKHBmeRrE1MzS84qz",
-			"expirationTime":null,
-			keys:{
-				p256dh:"BMCrojGuqQYWARLLY86LW_hTWP94Ab_G67jpKn6RrTLxL4m55OpB8KyNtzHCdAQ2Q1wsGBVsGFQdf0h7eUB4tSo=",
-				auth:"SEh3oGONb9nrw2OchmLC8w=="
-			}
-		};
 
-		var vapidKeys = {
-		  publicKey: publicKey,
-		  privateKey: privateKey
-		};
+		// var pushSubscription = {
+		// 	endpoint:"https://fcm.googleapis.com/fcm/send/dhwptTziGQA:APA91bFzuQNiHeslyZm5VHQ9flDrgwf0OKG5okaS9CfPynJtXHiOMyLORqBtbflbBpgT1MQHiRtEFLhWwBd-A851vgFV5iOoZbKqRbZwugngRB-enuJWe-n-IClsKHBmeRrE1MzS84qz",
+		// 	"expirationTime":null,
+		// 	keys:{
+		// 		p256dh:"BMCrojGuqQYWARLLY86LW_hTWP94Ab_G67jpKn6RrTLxL4m55OpB8KyNtzHCdAQ2Q1wsGBVsGFQdf0h7eUB4tSo=",
+		// 		auth:"SEh3oGONb9nrw2OchmLC8w=="
+		// 	}
+		// };
 
-		webpush.setVapidDetails(
-		  'https://damp-bayou-27809.herokuapp.com/',
-		  vapidKeys.publicKey,
-		  vapidKeys.privateKey
-		);
+		// var vapidKeys = {
+		//   publicKey: publicKey,
+		//   privateKey: privateKey
+		// };
 
-		webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+		// webpush.setVapidDetails(
+		//   'https://damp-bayou-27809.herokuapp.com/',
+		//   vapidKeys.publicKey,
+		//   vapidKeys.privateKey
+		// );
+
+		// webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
 	}
 };

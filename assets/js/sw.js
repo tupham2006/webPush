8ae2dc9e3df3495825263fde46747bfc0f9c8e45
tupham2006 <tupham20061995@gmail.com>
2017-12-12 12:29:42 +0700

    x

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,12 +1,12 @@
-// self.addEventListener('push', function(event) {
-// 	 event.waitUntil(
-// 	 	self.registration.showNotification('SalonHero', {
-// 	 		lang: 'vi',
-// 	 		body: 'Bạn có muốn hiển thị thông báo không?',
-// 	 	})
+self.addEventListener('push', function(event) {
+	 event.waitUntil(
+	 	self.registration.showNotification('SalonHero', {
+	 		lang: 'vi',
+	 		body: 'Bạn có muốn hiển thị thông báo không?',
+	 	})
 
-// 	 	.then(function(NotificationEvent) { 
-// 	 		console.log("NotificationEvent", NotificationEvent)
-// 	 	})
-// 	 );
-// });
+	 	.then(function(NotificationEvent) { 
+	 		console.log("NotificationEvent", NotificationEvent)
+	 	})
+	 );
+});

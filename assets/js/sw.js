1fb8a24b12a0d3d52a439c059b9d6697adab48dc
tupham2006 <tupham20061995@gmail.com>
2017-12-14 16:16:04 +0700

    push

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,28 +1,13 @@
-// self.addEventListener('push', function(event) {
-// 	console.log(event)
-// 	 event.waitUntil(
-// 	 	self.registration.showNotification('SalonHero', {
-// 	 		lang: 'vi',
-// 	 		body: event.data ? event.data : "Không hiển thị đưọc thông báo",
-// 	 	})
+self.addEventListener('push', function(event) {
+	console.log(event)
+	 event.waitUntil(
+	 	self.registration.showNotification('SalonHero', {
+	 		lang: 'vi',
+	 		body: event.data ? event.data : "Không hiển thị đưọc thông báo",
+	 	})
 
-// 	 	.then(function(NotificationEvent) { 
-// 	 		console.log("NotificationEvent", NotificationEvent)
-// 	 	})
-// 	 );
-// });
-// 
-
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
-
-firebase.initializeApp({messagingSenderId: "426490483066"});
-
-var messaging = firebase.messaging();
-messaging.setBackgroundMessageHandle(function(payload){
-	var title = 'SalonHero';
-	var option = {
-		body: payload.data.status
-	};
-	return shelf.registration.showNotification(title, option);
-})
+	 	.then(function(NotificationEvent) { 
+	 		console.log("NotificationEvent", NotificationEvent)
+	 	})
+	 );
+});

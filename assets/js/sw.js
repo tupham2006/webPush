eae8e4ad09eb0d13894e480fcf0cd6eb4a441dfb
tupham2006 <tupham20061995@gmail.com>
2017-12-15 14:39:42 +0700

    push

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,12 +1,9 @@
 // add Event Listener push to receive notification
 self.addEventListener('push', function(event) {
-	console.log(event);
-	console.log(event.data);
-	console.log(event.data.text());
 	 event.waitUntil(
 	 	self.registration.showNotification('SalonHero', {
 	 		lang: 'en',
-	 		body: event.data ? event.data : "Không hiển thị đưọc thông báo",
+	 		body: event.data ? event.data.text() : "Bạn có thông báo mới",
 	 	})
 
 	 	.then(function(NotificationEvent) { 

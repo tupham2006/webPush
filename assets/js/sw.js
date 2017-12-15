527c0c3cf6bfa847678beeb76908b0a7610e58ba
tupham2006 <tupham20061995@gmail.com>
2017-12-15 21:17:17 +0700

    push

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
+});
+
 // add Event Listener push to receive notification
 self.addEventListener('push', function(event) {
 	 event.waitUntil(

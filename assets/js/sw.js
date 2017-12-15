88bbe1177e46292331872a0deb56ea4616746c41
tupham2006 <tupham20061995@gmail.com>
2017-12-15 23:08:48 +0700

    push

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -16,3 +16,12 @@ self.addEventListener('push', function(event) {
 	 	})
 	 );
 });
+
+
+// self.addEventListener('notificationclick', function(e) {
+//   if (e.notification.tag !== 'user_visible_auto_notification') {
+//     // Open a same-origin page until https://code.google.com/p/chromium/issues/detail?id=457187
+//     // is resolved.
+//     clients.openWindow('redirect.html?url=' + encodeURIComponent(e.notification.tag));
+//   }
+// });

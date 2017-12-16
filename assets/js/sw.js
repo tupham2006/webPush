48d6cd160a4ddec8b838cc42427490be6587dbb0
tupham2006 <tupham20061995@gmail.com>
2017-12-16 07:43:22 +0700

    push

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,16 +1,18 @@
 self.addEventListener('install', function(e) {
-  e.waitUntil(self.skipWaiting());
   console.log('installed');
+  e.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', function(e) {
-  e.waitUntil(self.clients.claim());
   console.log('actived');
+  e.waitUntil(self.clients.claim());
 
 });
 
 // add Event Listener push to receive notification
 self.addEventListener('push', function(event) {
+  console.log('push');
+
 	 event.waitUntil(
 	 	self.registration.showNotification('SalonHero', {
 	 		lang: 'vi',
